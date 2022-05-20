bc1032eafa0df521a4ca155adf66d1c539dde224
DannyHung <danny.hung@ebti.com.tw>
2022-05-21 01:08:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+ <h1>hello vue.js</h1>
+ <!-- The solid style is implicit -->
+<font-awesome-icon icon="user-secret" />
+<button class="btn btn-primary">hello</button>
+</template>
+
+<style>
+@import './assets/base.css';
+
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+library.add(faUserSecret)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
